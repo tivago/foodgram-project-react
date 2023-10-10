dde79f2c1bf6853cd2cf9fed986e2d8a18cc5883
Дима Морозов <tivago96@yandex.ru>
2023-10-10 14:35:52 +0300

    push

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -140,10 +140,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = '/backend_static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 

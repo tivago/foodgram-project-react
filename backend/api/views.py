aa7268a90f2079a6f3e183ee577a783ab2b99f3b
tivago <tivago96@yandex.ru>
2023-10-12 21:51:27 +0300

    test

--- backend/api/views.py
+++ backend/api/views.py
@@ -149,8 +149,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
         shopping_cart = (
             ShoppingCart.objects.filter(user=request.user)
             .values(
-                'recipe__ingredients__name',
-                'recipe__ingredients__measurement_unit',
+                'recipe__recipesingredients__name',
+                'recipe__recipesingredients__measurement_unit',
             )
             .annotate(amount=Sum('recipe__recipesingredients__amount'))
             .order_by()
@@ -158,9 +158,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
         page = ['Список покупок:\n--------------']
         for index, recipe in enumerate(shopping_cart, start=1):
             page.append(
-                f'{index}. {recipe["recipe__ingredients__name"]} - '
+                f'{index}. {recipe["recipe__recipesingredients__name"]} - '
                 f'{recipe["amount"]} '
-                f'{recipe["recipe__ingredients__measurement_unit"]}.',
+                f'{recipe["recipe__recipesingredients__measurement_unit"]}.',
             )
         response = HttpResponse(page, content_type='text/csv')
         response['Content-Disposition'] = (

72250e4a32a37b031ae5269bee7a12a58acc19e2
Дима Морозов <tivago96@yandex.ru>
2023-10-12 17:19:47 +0300

    delete debug

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -25,14 +25,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'debug_toolbar',
     'rest_framework.authtoken',
     'djoser',
     'django_filters',
 ]
 
 MIDDLEWARE = [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',

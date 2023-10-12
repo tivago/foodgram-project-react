6333f899d4a6a5f17d5001fc40df1bfc15bc4e7f
tivago <tivago96@yandex.ru>
2023-10-12 23:13:34 +0300

    test

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -2,4 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPagination(PageNumberPagination):
+    page_size = 6
     page_size_query_param = 'limit'
+    page_query_param = 'page'

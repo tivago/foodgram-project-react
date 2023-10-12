867acdef1b6bd4b1fce4c7d10d8fc6bdc760de2b
Дима Морозов <tivago96@yandex.ru>
2023-10-12 14:33:28 +0300

    test

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,14 @@
+import io
+
 from django.db.models import Sum
 from django.http import FileResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
-from recipes.models import (Favorite, Ingredient, IngredientInRecipe, Recipe,
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfgen import canvas
+from recipes.models import (Favorite, Ingredient, Recipe,
                             ShoppingCart, Subscription, Tag)
 from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
@@ -15,9 +20,10 @@ from .filters import IngredientSearchFilter, RecipeFilter
 from .pagination import CustomPagination
 from .permissions import IsAdminOrReadOnly, IsAuthorOrReadOnly
 from .serializers import (IngredientSerializer, PasswordSerializer,
-                          RecipeMinifieldSerializer, RecipePostSerializer,
-                          RecipeSerializer, SubscriptionsSerializer,
+                          RecipeMinifieldSerializer, SubscriptionsSerializer,
                           TagSerializer, UserSerializer)
+
+FILENAME = 'shoppingcart.pdf'
 
 
 class CreateUserViewSet(UserViewSet):
@@ -134,32 +140,45 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-    def get_serializer_class(self):
-        if self.request.method == 'GET':
-            return RecipeSerializer
-        else:
-            return RecipePostSerializer
-
-    @action(
-        detail=False,
-        methods=('get',),
-        permission_classes=(IsAuthenticated, )
-    )
     def download_shopping_cart(self, request):
-        ingredients = (IngredientInRecipe.objects.filter(
-            recipe__shopping_carts__user=request.user).values(
-            'ingredient__name',
-            'ingredient__measurement_unit',
-        ).annotate(amount=Sum('amount')))
-        wishlist = []
-        for ingredient in ingredients:
-            wishlist.append('{name} - {amount} {m_unit}\n'.format(
-                name=ingredient.get('ingredient__name'),
-                amount=ingredient.get('amount'),
-                m_unit=ingredient.get('ingredient__measurement_unit')
-            ))
-        response = FileResponse(wishlist, content_type='text/plain')
-        return response
+        """Качаем список с ингредиентами."""
+
+        buffer = io.BytesIO()
+        page = canvas.Canvas(buffer)
+        pdfmetrics.registerFont(TTFont('Vera', 'Vera.ttf'))
+        x_position, y_position = 50, 800
+        shopping_cart = (
+            request.user.shopping_cart.recipe.
+            values(
+                'ingredients__name',
+                'ingredients__measurement_unit'
+            ).annotate(amount=Sum('recipe__amount')).order_by())
+        page.setFont('Vera', 14)
+        if shopping_cart:
+            indent = 20
+            page.drawString(x_position, y_position, 'Cписок покупок:')
+            for index, recipe in enumerate(shopping_cart, start=1):
+                page.drawString(
+                    x_position, y_position - indent,
+                    f'{index}. {recipe["ingredients__name"]} - '
+                    f'{recipe["amount"]} '
+                    f'{recipe["ingredients__measurement_unit"]}.')
+                y_position -= 15
+                if y_position <= 50:
+                    page.showPage()
+                    y_position = 800
+            page.save()
+            buffer.seek(0)
+            return FileResponse(
+                buffer, as_attachment=True, filename=FILENAME)
+        page.setFont('Vera', 24)
+        page.drawString(
+            x_position,
+            y_position,
+            'Cписок покупок пуст!')
+        page.save()
+        buffer.seek(0)
+        return FileResponse(buffer, as_attachment=True, filename=FILENAME)
 
 
 class IngredientViewSet(viewsets.ReadOnlyModelViewSet):

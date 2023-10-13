d4aad3228e4eec3e39ce3873c3455ddc1608d734
tivago <tivago96@yandex.ru>
2023-10-13 23:48:06 +0300

    rev

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -31,7 +31,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',

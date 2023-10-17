a4992115ca10e1c6b1cbce944f16182bf125b139
tivago <tivago96@yandex.ru>
2023-10-17 20:19:02 +0300

    test

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -74,9 +74,6 @@ DATABASES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-valida
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',

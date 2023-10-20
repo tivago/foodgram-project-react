5f07eb92d427c1619de3c295837d720a7285236b
Дима Морозов <tivago96@yandex.ru>
2023-10-20 13:01:58 +0300

    testing

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -63,6 +63,8 @@ class UserSerializer(UserCreateSerializer):
 
     def get_is_subscribed(self, obj):
         request = self.context.get('request')
+        if not request or request.user.is_authenticated:
+            return False
         return Subscription.objects.filter(
             user=request.user, author=obj
         ).exists()
@@ -152,10 +154,14 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     def get_is_favorited(self, obj):
         request = self.context.get('request')
+        if request is None or request.user.is_anonymous:
+            return False
         return Favorite.objects.filter(user=request.user, recipe=obj).exists()
 
     def get_is_in_shopping_cart(self, obj):
         request = self.context.get('request')
+        if request is None or request.user.is_anonymous:
+            return False
         return ShoppingCart.objects.filter(
             user=request.user, recipe=obj
         ).exists()
